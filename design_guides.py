c0e3062e7848ae79b279876b3eb1ec85ee0e84ab
Bion Howard <bion@bitpharma.com>
2020-03-18 13:28:31 -0400

    feat(tries): add tries and test em

--- design_guides.py
+++ design_guides.py
@@ -1,14 +1,20 @@
 from Bio import AlignIO, SeqIO
+from datetime import datetime
 from Bio.Seq import Seq
 import itertools
+import pickle
 import redis
 import os
-from datetime import datetime
 
 # length of the CRISPR guide RNA
 K = 28
 # path to a fasta file with host sequences to avoid
 HOST_PATH = os.path.join("host", "lung-tissue-gene-cds.fa")
+# ending token for tries
+END = "*"
+# path to pickle / save the trie
+LOAD_PICKLE = True
+TRIE_PATH = "trie.pkl"
 # path to alignment and id for the sequence to delete
 TARGET_PATH = os.path.join("alignments", "HKU1+MERS+SARS+nCoV-Consensus.clu")
 TARGET_ID = "nCoV"
@@ -21,11 +27,10 @@ PROMOTER_PATH = os.path.join("parts", "pdpn_1_promoter.fa")
 DR_SEQUENCE_PATH = os.path.join("parts", "crispr", "dr_sequence_bz_short.fa")
 TAIL_PATH = os.path.join("parts", "tail.fa")
 # path for output
-OUTFILE_PATH = os.path.join(
-    "guides", "SARS-nCoV-2_consensus_conserved_watson_crick_guides_RNA.csv")
+OUTFILE_PATH = os.path.join("guides", "trie_guides.csv")
 
 r = redis.Redis(host='localhost', port=6379)
-trie = {}
+trie = {} if not LOAD_PICKLE else pickle.load(open(TRIE_PATH, "rb"))
 
 # helpers
 def all_equal(arr):
@@ -47,15 +52,17 @@ def getKmers(sequence, k, step):
 
 
 def index(kmer):
-	node = trie
-	for base in kmer:
-	    if base not in node:
-	        node[base] = {}
-	    node = node[base]
+    node = trie
+    for base in kmer:
+        if base not in node:
+            node[base] = {}
+        node = node[base]
+    node[END] = END
+
 
 def _find(node, path, kmer, d):
 	if not kmer:
-	    if len(path) is K:
+	    if END in node:
 	        yield (path, d)
 	    return
 	base, suffix = kmer[0], kmer[1:]
@@ -74,26 +81,29 @@ def find(kmer):
 def host_has(kmer):
 	matches = list(find(kmer))
 	should_avoid = len(matches) > 0
-	notice = "avoid" if should_avoid else "allow"
+	notice = "AVOID" if should_avoid else "allow"
 	print(notice, kmer, "matches", matches)
 	return should_avoid
 
-# TODO: rewrite with "index" function
+
 def make_hosts():
-    rcount, kcount = 0, 0
+    if LOAD_PICKLE:
+        return
     with open(HOST_PATH, "r") as host_file:
-        for record in SeqIO.parse(host_file, "fasta"):
-            rcount = rcount + 1
-            for kmer in getKmers(record.seq.lower(), K, 1):
-                kcount = kcount + 1
-                print(rcount, kcount, kmer)
-                r.sadd("hosts", str(kmer))
+        for rcount, record in enumerate(SeqIO.parse(host_file, "fasta")):
+            for kcount, kmer in enumerate(getKmers(record.seq.lower(), K, 1)):
+                kmer_string = str(kmer)
+                print(rcount, kcount, kmer_string)
+                r.sadd("hosts", kmer_string)
+                index(kmer_string)
+    with open(TRIE_PATH, "wb+") as trie_file:
+        pickle.dump(trie, trie_file)
 
 
 def make_targets():
     alignment = AlignIO.read(TARGET_PATH, "clustal")
-    sequence_ids = [seq.id for seq in alignment]
-    index_of_target = sequence_ids.index(TARGET_ID)
+    seq_ids = [seq.id for seq in alignment]
+    index_of_target = seq_ids.index(TARGET_ID)
     alignment_length = alignment.get_alignment_length()
     conserved = [1 if all_equal(
         [seq[i] for seq in alignment]) else 0 for i in range(alignment_length)]
@@ -106,46 +116,31 @@ def make_targets():
         n_conserved = sum(conserved[start:start+K])
         print(f"{kmer} at {start} has {int(n_conserved)} conserved bases")
         r.zadd("targets", {kmer: n_conserved})
-    most_conserved_kmer = r.zrevrangebyscore(
-        "targets", 9001, 0, withscores=True, start=0, num=1)[0]
+    most = r.zrevrangebyscore("targets", 9001, 0, withscores=True, start=0, num=1)[0]
     print(
-        f"the most conserved {K}mer is {most_conserved_kmer[0].decode()} with {int(most_conserved_kmer[1])} bases conserved between {sequence_ids}")
+        f"the most conserved {K}mer {most[0].decode()} has {int(most[1])} bases conserved in {seq_ids}")
 
 
-# TODO: rewrite with "host_has" function
 def predict_side_effects():
     targets = r.zrevrangebyscore("targets", 9001, 0)
-    hosts = r.smembers("hosts")
     for target in targets:
-        for host in hosts:
-            d = sum([0 if target[n] is host[n] else 1 for n in range(K)])
-            print(f"d({target.decode()}, {host.decode()}) = {d}")
-            if d < CUTOFF:
-                print("found potential mismatch:",
-                      target.decode(), host.decode())
-                break
-        print("no side effects found for: ", target)
-        r.zadd("good_targets", {target: r.zscore("targets", target)})
-
-
-def make_plasmids():
-    pol3_promoter = read_fasta(PROMOTER_PATH)
-    dr_sequence = read_fasta(DR_SEQUENCE_PATH)
-    # tail = read_fasta(TAIL_PATH)
-    good_targets = r.zrevrangebyscore("good_targets", 9001, 0)
-    timestamp = datetime.now()
-    for i, target in enumerate(good_targets):
-        guide = target.reverse_complement()
-        score = r.zscore(target)
-        title = f"{i}_{guide}_{score}_{timestamp}"
-        transcripts = [pol3_promoter, guide, dr_sequence]
-        plasmid = "".join(transcripts)
-        write_fasta(title, plasmid)
-
+        t = target.decode()
+        should_avoid = host_has(t)
+        if should_avoid:
+            continue
+        r.zadd("good_targets", {target: r.zscore("targets", t)})
+    with open(OUTFILE_PATH, "w+") as outfile:
+        for k, good_target in enumerate(r.zrevrangebyscore("good_targets", 90, 0)):
+            good_target_string = good_target.decode()
+            print("good target", k, good_target_string)
+            outfile.write(good_target_string + "\n")
+    print(f"saved {r.zcard('good_targets')} good targets at {OUTFILE_PATH}")
 
 if __name__ == "__main__":
-    # make_hosts()
-    # make_targets()
-    # predict_side_effects()
-    make_plasmids()
+    make_hosts()
+    # test the trie lookup works
+    for i in range(5):
+        host_has(r.srandmember("hosts").decode())
+    make_targets()
+    predict_side_effects()
 
