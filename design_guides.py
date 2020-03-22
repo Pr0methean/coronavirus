eb4603706c2537b840f0661dcdca27f21a4dd5c5
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-21 18:51:33 -0700

    Bug fix

--- design_guides.py
+++ design_guides.py
@@ -73,7 +73,7 @@ def index(kmer, db=leveldb):
 
 def add_to_bytes_as_set(char, dest):
     if dest == EMPTY_BYTES:
-        return bytesu(char)
+        return bytes([ord(char)])
     new_value_set = set(dest)
     new_value_set.add(ord(char))
     new_value = bytes(sorted(new_value_set))
