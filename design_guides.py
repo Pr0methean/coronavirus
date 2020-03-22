66a41d464aac8995010fcfb23875fe3974e2c5ac
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2020-03-21 17:18:45 -0700

    Revert skip record
    Accidentally committed; was used on local machine to resume an unfinished run from the middle

--- design_guides.py
+++ design_guides.py
@@ -105,9 +105,7 @@ def make_hosts(input_path=HOST_PATH, db=r, ldb=leveldb):
         return
     trie.clear()
     with open(input_path, "r") as host_file:
-        records = enumerate(SeqIO.parse(host_file, "fasta"))
-        [next(records) for x in range(116936)]
-        for rcount, record in records:
+       for rcount, record in enumerate(SeqIO.parse(host_file, "fasta")):
             for kmer in getKmers(record.seq.lower(), K, 1):
                 kmer_string = str(kmer)
                 db.sadd("hosts", kmer_string)
