3dce1574e494a449eebec77fdcf61197a455661a
Bion Howard <bion@bitpharma.com>
2020-04-06 16:43:45 -0400

    feat(plasmid): add plasmids folder and CasRx plasmid

--- plasmid.py
+++ plasmid.py
@@ -6,30 +6,38 @@ from Bio import SeqIO
 import os
 
 # path to plasmid backbone genbank
-# BACKBONE_PATH = os.path.join("data", "parts", "AAV-pCMV-EGFP.gb")
-# start of EGFP (one-indexed!)
-# EGFP_START, EGFP_END = 788, 1507
+BACKBONE_PATH = os.path.join("data", "parts", "AAV-pCMV-EGFP.gb")
+# end of 3' itr (one-indexed!)
+THREE_PRIME_ITR_END = 1926
 # path to 5 prime ITR
 FIVE_PRIME_ITR_PATH = os.path.join("data", "parts", "AAV-5prime-itr.fa")
 # path to CMV promoter/enhancer
-PROMOTER_PATH = os.path.join("data", "parts", "pCMV.fa")
+PCMV_PATH = os.path.join("data", "parts", "pCMV.fa")
 # path to kozak ribosomal binding site
 KOZAK_PATH = os.path.join("data", "parts", "kozak.fa")
 # path to CRISPR DNA
 CRISPR_PATH = os.path.join("data", "parts", "crispr", "casrx-dna.fa")
 # path to polyA tail
 POLYA_PATH = os.path.join("data", "parts", "sv40-poly-a.fa")
+# path to pU6 promoter
+PU6_PATH = os.path.join("data", "parts", "pU6.fa")
+# path to direct repeat (DR)
+DR_PATH = os.path.join("data", "parts", "crispr", "casrx-dr.fa")
 # path to 3' ITR
 THREE_PRIME_ITR_PATH = os.path.join("data", "parts", "AAV-3prime-itr.fa")
 # list of paths
 PATHS = [
     FIVE_PRIME_ITR_PATH,
-    PROMOTER_PATH,
+    PCMV_PATH,
     KOZAK_PATH,
     CRISPR_PATH,
     POLYA_PATH,
-    THREE_PRIME_ITR_PATH
+    THREE_PRIME_ITR_PATH,
+    PU6_PATH
 ]
+GUIDE_FILE = "trie_guides_26_full_transcriptome.csv"
+GUIDES = [l.rstrip() for l in open(os.path.join("data", "guides", GUIDE_FILE))]
+K = len(GUIDES[0])
 
 def splice(prior, insert, start, end):
     s, e = start - 1, end - 1
@@ -37,20 +45,23 @@ def splice(prior, insert, start, end):
     assert len(prior_left) + len(removed) + len(prior_right) == len(prior)
     return prior_left + insert + prior_right
 
-def add_crispr():
-    backbone = SeqIO.read(BACKBONE_PATH, "genbank").seq.lower()
-    print(backbone)
-    crispr = SeqIO.read(CRISPR_PATH, "fasta").seq.lower()
-    print(crispr)
-    spliced = splice(backbone, crispr, EGFP_START, EGFP_END)
-    print(spliced.index(crispr))
-    return spliced
+def design_plasmid():
+    insert = "".join([str(load_fasta(p)) for p in PATHS])
+    dr = load_fasta(DR_PATH)
+    g = 0
+    while len(insert) < 4700 - len(dr) - K:
+        insert = insert + dr + GUIDES[g]
+        g = g + 1
+    right_side = SeqIO.read(BACKBONE_PATH, "genbank").seq.lower()[THREE_PRIME_ITR_END:]
+    plasmid_sequence = insert + right_side
+    return plasmid_sequence, len(insert), g
 
 def load_fasta(path, fmt="fasta"):
     return SeqIO.read(path, fmt).seq.lower()
 
 if __name__ == "__main__":
-    sequences = [load_fasta(p) for p in PATHS]
-    insert_size = sum(map(len, sequences))
-    print(insert_size)
+    plasmid_sequence, insert_size, g = design_plasmid()
+    print(f">AAV-pCMV-CasRx_w_{g}x{K}_anti-SARS-CoV-2_gRNA_insert_size_{insert_size}")
+    print(plasmid_sequence)
+    
 
